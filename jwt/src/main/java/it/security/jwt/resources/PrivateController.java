647955cff337c63e2aca8f9d7a0f24a9fb4434fe
c_giacomo <c.giacomo@hotmail.it>
2020-11-26 20:42:13 +0100

    fix for Angular front-end

--- jwt/src/main/java/it/security/jwt/resources/PrivateController.java
+++ jwt/src/main/java/it/security/jwt/resources/PrivateController.java
@@ -1,18 +1,30 @@
 package it.security.jwt.resources;
 
+import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @RestController
 @RequestMapping("/api/private")
 public class PrivateController {
 
-    @GetMapping
-    public String getMessage() {
+   @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public Value getMessage() {
     	Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        return "Hello " + authentication.getPrincipal() + " from private API controller";
+    	Value value = new Value();
+    	value.setString("Hello " + authentication.getPrincipal() + " from private API controller");
+        return value;
     }
+}
+
+@Getter
+@Setter
+class Value {
+	private String string;
 }
